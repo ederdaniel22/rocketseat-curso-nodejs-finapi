a4112b0f6cdf4bed3e98c5eda4bf2211a2b6b381
ederdaniel22 <eder.infonet@gmail.com>
2021-11-01 20:40:18 -0400

    Criaçao de account, statement testes insomnia

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const { v4: uuidv4 } = require("uuid");
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 const customers = [];
 /**
@@ -13,16 +13,36 @@ const customers = [];
  * statement []
  */
 
-app.post("/account", (request, response)=> {
-    const {cpf, name} = request.body;
-    const id = uuidv4();
-    customers.push({
-        cpf, 
-        name,
-        id,
-        statement: []
-    });
-    return response.status(201).send();
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf,
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: 'Customer already exists!' });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+  return response.status(201).send();
 });
 
-app.listen(8080);
+app.get('/statement/', (request, response) => {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: 'Customer not found' });
+  }
+
+  return response.json(customer.statement);
+});
+
+app.listen(3333);
